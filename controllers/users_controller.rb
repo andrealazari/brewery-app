700f4bb2e3949d1117cb26a0a6beb6f6c2cec7ed
andrealazari <a_lazari@hotmail.com>
2022-05-23 17:23:13 +1000

    adding sign up check

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -5,7 +5,7 @@ get '/signup' do
         }
     else 
         erb :'users/new', locals: {
-            sign_up_message: 'This user has alredy a brewery! Please try again.'
+            sign_up_message: 'This user has already a brewery! Please try again.'
         }
     end
 end
@@ -15,14 +15,37 @@ post '/user' do
     email = params['email']
     password = params['password']
 
-    emails_already_created = all_users_emails_created().to_a
-    emails_already_created.each do |user_email|
-        if user_email.has_value? email
-            redirect '/signup?sign_up_message=true'
-            break
-        else
-            create_user(name, email, password)
-            redirect '/'
-        end
+    # emails_already_created = all_users_emails_created().to_a
+    # emails_already_created.each do |user_email|
+    #     if user_email.has_value? email
+    #         redirect '/signup?sign_up_message=true'
+    #         break
+    #     else
+    #         create_user(name, email, password)
+    #         redirect '/'
+    #     end
+    # end
+
+    if find_user_by_name(name).to_a.count > 0
+        redirect '/signup?sign_up_message=true'
+    elsif find_user_by_email(email).to_a.count > 0
+        redirect '/signup?sign_up_message=true'
+    else
+        user = create_user(name, email, password) 
+        
+        session['user_id'] = user['id']
+        
+        redirect '/my_page'
     end
+
+    if params['login_message'] == nil
+        erb :'sessions/new', locals: {
+            login_message: ''
+        }
+    else 
+        erb :'sessions/new', locals: {
+            login_message: 'Wrong email/password! Are you already drunk?'
+        }
+    end
+
 end
--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,7 @@
 def create_user(name, email, password)
     password_digest = BCrypt::Password.create(password)
 
-    run_sql("INSERT INTO users(name, email, password_digest) VALUES($1, $2, $3)", [name, email, password_digest])
+    run_sql("INSERT INTO users(name, email, password_digest) VALUES($1, $2, $3) RETURNING *", [name, email, password_digest]).to_a[0]
 end
 
 def find_user_by_email(email)
@@ -16,6 +16,16 @@ end
 
 def find_user_by_id(id)
     run_sql("SELECT * FROM users WHERE id = $1", [id])[0]
+end
+
+def find_user_by_name(name)
+    users = run_sql("SELECT * FROM users WHERE name = $1", [name])
+
+    if users.to_a.count > 0
+        users[0]
+    else
+        nil
+    end
 end
 
 def all_users_emails_created()
